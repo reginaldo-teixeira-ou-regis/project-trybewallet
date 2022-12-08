4dfc4007c86ee3d4c8826810d833ace5f8c7f13e
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2022-12-08 19:18:12 -0300

    Requisite 1 finished

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login as loginAction } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const { dispatch, history } = this.props;
+    return (
+      <div className="Login">
+        <h3 className="text-center">Login</h3>
+        <section className="login-inputs" />
+        <form className="formLogin">
+          <label htmlFor="email">
+            <input
+              type="email"
+              placeholder="Digite seu e-mail"
+              name="email"
+              id="email"
+              data-testid="email-input"
+              onChange={ (e) => this.setState({ email: e.target.value }) }
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              name="password"
+              id="password"
+              data-testid="password-input"
+              onChange={ (e) => this.setState({ password: e.target.value }) }
+            />
+          </label>
+          <button
+            type="button"
+            onClick={ () => dispatch(loginAction({ email, password }))
+              && history.push('/carteira') }
+            disabled={ password.length < +'6' || !email.match(
+              /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm,
+            ) }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape().isRequired,
+};
+
+export default connect()(Login);
